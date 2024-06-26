d0da5c8178244025144e7fc7f138da5151633f89
Riris Iyut Roito Siregar <ririsiyut03@gmail.com>
2024-06-26 09:55:43 +0700

    update 1

--- app_streamlit.py
+++ app_streamlit.py
@@ -7,15 +7,15 @@ angka_pertama = st.number_input('Masukkan angka pertama:', min_value=0, value=0)
 angka_kedua = st.number_input('Masukkan angka kedua:', min_value=0, value=0)
 
 if st.button("convert"):
-    try:
+    # try:
 
-        response = requests.post(
-            "https://e1b3-103-82-14-56.ngrok-free.app",
-            json={"angka_pertama": angka_pertama, "angka_kedua": angka_kedua}
-        )
-    except Exception as e :
-        print("Exception", e)
-        st.error("Error in conversion")
+    response = requests.post(
+    "https://e1b3-103-82-14-56.ngrok-free.app",
+    json={"angka_pertama": angka_pertama, "angka_kedua": angka_kedua}
+    )
+    # except Exception as e :
+    #     print("Exception", e)
+    #     st.error("Error in conversion")
     
     if response.status_code == 200:
         result = response.json()
